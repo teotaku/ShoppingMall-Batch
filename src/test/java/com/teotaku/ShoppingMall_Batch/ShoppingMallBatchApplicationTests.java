eec19438f0b9fd442f775493ddf5ce60fe197bca
teotaku <dongik9467@naver.com>
2025-12-29 16:04:57 +0900

    first commit

---
+++ src/test/java/com/teotaku/ShoppingMall_Batch/ShoppingMallBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teotaku.ShoppingMall_Batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
