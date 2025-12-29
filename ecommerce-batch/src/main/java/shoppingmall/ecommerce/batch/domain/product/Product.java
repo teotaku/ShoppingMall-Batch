c09f784daf70367fcca2d9f5bacf968324748fda
teotaku <dongik9467@naver.com>
2025-12-29 18:19:28 +0900

    feat: ëbatch 모듈 분리

---
+++ ecommerce-batch/src/main/java/shoppingmall/ecommerce/batch/domain/product/Product.java
@@ -0,0 +1,35 @@
+package shoppingmall.ecommerce.batch.domain.product;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Product {
+
+  private String productId;
+  private Long sellerId;
+
+
+  private String category;
+  private String name;
+  private LocalDate salesStartDate;
+  private LocalDate salesEndDate;
+  private ProductStatus productStatus;
+
+  private String brand;
+  private String manufacturer;
+
+  private int salesPrice;
+  private int stockQuantity;
+
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
+}
